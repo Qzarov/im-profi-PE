1bf678f622c7ee9cf61ef496d3258fbc1e717f37
Yaroslav Paroshin <qzarov@mail.ru>
2022-03-10 10:19:18 +0300

    Create proj, update .gitignore

---
+++ im-profi-SE/im-profi-SE.cpp
@@ -0,0 +1,12 @@
+﻿// im-profi-SE.cpp: определяет точку входа для приложения.
+//
+
+#include "im-profi-SE.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
